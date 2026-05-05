68c5a0a9313e17dcb0ae60e346dccaabc334711e
krish-maurya <mauryakrish734@gmail.com>
2026-05-05 14:43:25 +0530

    json file added

--- server/server.ts
+++ server/server.ts
@@ -95,8 +95,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
